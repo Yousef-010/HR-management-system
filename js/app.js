6f09f347cf3d9e60e6f9860bbcb7f12c01b17365
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-04-26 00:33:17 +0300

    resubmitting

--- js/app.js
+++ js/app.js
@@ -9,9 +9,24 @@ function Employee( e_id,e_fullName,e_department,e_level,e_imgUrl ){
   this.department = e_department,
   this.level = e_level,
   this.imgUrl = e_imgUrl,
+  this.salary=0
 
   E_array.push(this)
 }
+
+Employee.prototype.getsal= function(){
+  if (this.level==='Junior'){
+    this.salary= Math.floor(Math.random()*(1000-500)+500)
+  }
+  if (this.level==='Senior'){
+    this.salary= Math.floor(Math.random()*(1500-1000)+1000)
+  }
+  if (this.level==='Mid-Senior'){
+    this.salary= Math.floor(Math.random()*(2000-1500)+1500)
+  }
+  this.salary= this.salary - (this.salary/100)*7.5
+}
+
 
 
 let bttn = document.getElementById('bttn')
@@ -31,31 +46,45 @@ Employee.prototype.getId= function(min,max){
 
 let M_main = document.getElementById('main')
 let E_container = document.getElementById('container')
- let card = document.getElementById('card')
+
 
 Employee.prototype.render = function () { 
+    let saf = document.getElementById('saf')
+     
+    let card = document.createElement('div')
+    card.classList.add('card')
 
-     let image = document.createElement('img')
-     image.width='350'
-     image.height='200'
-     let name = document.createElement('h1')
-     let Id = document.createElement('h2')
-     let dep = document.createElement('h3')
-     let lev = document.createElement('h4')
+    for (let i = 0; i < 1 ; i++) {
+      
+      let image = document.createElement('img')
+      image.width='350'
+      image.height='200'
+      let name = document.createElement('h1')
+      let Id = document.createElement('h2')
+      let dep = document.createElement('h3')
+      let lev = document.createElement('h4')
+      let sal = document.createElement('h5')
+ 
+ 
+     image.setAttribute('src',this.imgUrl)
+     name.textContent= 'Name : '+ this.fullName
+     Id.textContent= '-ID :'+ this.id
+     dep.textContent= 'Deperatment: '+ this.department
+     lev.textContent= 'Level : '+ this.level
+     sal.textContent = this.salary+'$'
+ 
+ 
+     card.appendChild(image)
+     card.appendChild(name)
+     card.appendChild(Id)
+     card.appendChild(dep)
+     card.appendChild(lev)
+     card.appendChild(sal)
+
+    }
+    saf.appendChild(card)
 
 
-    image.setAttribute('src',this.imgUrl)
-    name.textContent= 'Name : '+ this.fullName
-    Id.textContent= '-ID :'+ this.id
-    dep.textContent= 'Deperatment: '+ this.department
-    lev.textContent= 'Level : '+ this.level
-
-
-    card.appendChild(image)
-    card.appendChild(name)
-    card.appendChild(Id)
-    card.appendChild(dep)
-    card.appendChild(lev)
 
 
 }
@@ -199,11 +228,14 @@ const hadi = new Employee(
 
 )
 
+
+
 function renderAll(){
 
 for (let i = 0; i < E_array.length; i++) {
 
     E_array[i].getId(1000,9999)
+    E_array[i].getsal()
     E_array[i].render()
 
 }
@@ -212,10 +244,7 @@ for (let i = 0; i < E_array.length; i++) {
 renderAll()
 
 
-bttn.addEventListener('click', say_hi)
-function say_hi(){
-    alert('Lsets Start Adding Employees ...')
-}
+ 
 
 form.addEventListener('submit',addsubmit)
 
